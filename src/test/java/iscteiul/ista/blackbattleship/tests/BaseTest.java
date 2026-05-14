ccd5e55e1d442ea3eb7ea090314747fe7b6f26d1
mirfa <mirfa1@iscte-iul.pt>
2026-05-14 15:08:09 +0100

    Convergencia de paginas

--- src/test/java/iscteiul/ista/blackbattleship/tests/BaseTest.java
+++ src/test/java/iscteiul/ista/blackbattleship/tests/BaseTest.java
@@ -1,4 +1,5 @@
 package iscteiul.ista.blackbattleship.tests;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 
@@ -10,11 +11,15 @@ public class BaseTest {
         open("https://papergames.io/en/battleship");
         acceptCookiesIfPresent();
     }
+    @AfterEach
+    public void tearDown() {
+        // Mantém o browser aberto por uns segundos após o teste para veres o resultado final
+        sleep(3000);
+    }
 
-    private void acceptCookiesIfPresent() {
-        var cookieButton = $(By.xpath("//*[self::button or self::a][contains(.,'Consent')]"));
-        if (cookieButton.exists()) {
-            cookieButton.click();
+    public void acceptCookiesIfPresent() {
+        if ($(".qc-cmp2-summary-buttons").exists()) { // Seletor comum de cookies
+            $(By.xpath("//button[contains(.,'Agree')]")).click();
         }
     }
 }
--- src/test/java/iscteiul/ista/blackbattleship/tests/US05_PlayWithFriend.java
+++ src/test/java/iscteiul/ista/blackbattleship/tests/US05_PlayWithFriend.java
@@ -1,19 +1,27 @@
 package iscteiul.ista.blackbattleship.tests;
 
-import com.codeborne.selenide.Configuration;
-import iscteiul.ista.blackbattleship.pages.MatchSetupPage;
+import iscteiul.ista.blackbattleship.pages.HomePage;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 public class US05_PlayWithFriend extends BaseTest {
-    MatchSetupPage page = new MatchSetupPage();
+    HomePage page = new HomePage();
     @Test
     public void playWithFriendTest() {
 
         page.playWithFriend.shouldBe(visible)
                 .click();
+
+        // Limpar e preencher o campo do nickname
+        page.inputNickname.shouldBe(visible).setValue("OdivelasPlayer");
+
+        // Clicar em continuar
+        page.btnContinue.shouldBe(visible).click();
+
+        // 4. Asserção (Validação)
+        // Para validar que o teste foi um sucesso, podemos verificar se o botão "Continue" desapareceu
+        // ou se fomos redirecionados para o ecrã seguinte.
+        page.btnContinue.shouldNotBe(visible);
     }
 }
