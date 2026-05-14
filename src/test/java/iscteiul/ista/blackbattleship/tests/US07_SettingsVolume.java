d1ab7dcc01790d132504359e18f28e741e5c80a5
mirfa <mirfa1@iscte-iul.pt>
2026-05-14 15:08:24 +0100

    melhoria dos testes

--- src/test/java/iscteiul/ista/blackbattleship/tests/US07_SettingsVolume.java
+++ src/test/java/iscteiul/ista/blackbattleship/tests/US07_SettingsVolume.java
@@ -4,10 +4,11 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import iscteiul.ista.blackbattleship.pages.SettingsPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class US07_SettingsVolume extends BaseTest {
 
@@ -19,6 +20,28 @@ public class US07_SettingsVolume extends BaseTest {
         page.settingsButton.click();
 
         page.volumeSlider.shouldBe(Condition.visible);
+        // obter valor inicial
+        Long initialValue = executeJavaScript(
+                "return arguments[0].value;",
+                page.volumeSlider
+        );
+
+        // alterar slider
+        executeJavaScript(
+                "arguments[0].value = 80;" +
+                        "arguments[0].dispatchEvent(new Event('input'));",
+                page.volumeSlider
+        );
+
+
+        // obter novo valor
+        Long newValue = executeJavaScript(
+                "return arguments[0].value;",
+                page.volumeSlider
+        );
+
+        // verificar mudança
+        assertNotEquals(initialValue, newValue);
     }
 
 }
